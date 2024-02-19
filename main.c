1dcc071f125b79024fc9a00d19f27f02e540f751
3RBNK <dawidenkokirill@gmail.com>
2024-02-19 15:47:53 +0300

    add swap rows/columns and sort rows/columns

--- main.c
+++ main.c
@@ -3,8 +3,26 @@
 #include "data_structures/matrix/matrix.h"
 
 
-int main() {
+int get_sum(int* a, int n) {
+    int result = 0;
 
+    for (int i = 0; i < n; i++)
+        result += a[i];
+
+    return result;
+}
+
+
+
+
+int main() {
+    matrix a = get_mem_matrix(5, 3);
+
+    input_matrix(&a);
+
+    selection_sort_cols_matrix_by_col_criteria(&a, get_sum);
+
+    output_matrix(a);
 
     return 0;
 }
